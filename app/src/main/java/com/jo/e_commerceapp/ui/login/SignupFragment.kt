459e7a4ef3a27f6ae4d5db3261e58bf31907edcc
Jogy Felix <jogyfelix1@gmail.com>
2022-02-12 00:10:09 +0530

    added login and sign up fragments with basic components

---
+++ app/src/main/java/com/jo/e_commerceapp/ui/login/SignupFragment.kt
@@ -0,0 +1,30 @@
+package com.jo.e_commerceapp.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import coil.load
+import com.jo.e_commerceapp.R
+import com.jo.e_commerceapp.databinding.FragmentSignupBinding
+
+
+class SignupFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignupBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignupBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.backgroundImage.load(R.drawable.login_bg)
+    }
+}
